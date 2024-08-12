d6a1cf751058f52cf64124f71d70051e3f154411
user <novikova.rita@yandex.ru>
2024-08-12 23:58:14 +0300

    экран authentification и sms начальная верстка.

--- app/src/main/java/com/example/connectme/ui/auth/PhoneAuthScreen.kt
+++ app/src/main/java/com/example/connectme/ui/auth/PhoneAuthScreen.kt
@@ -1,2 +1,66 @@
 package com.example.connectme.ui.auth
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.connectme.ui.theme.ConnectMeTheme
+
+@Composable
+fun PhoneAuthScreen(modifier: Modifier) {
+    var phoneNumber by remember { mutableStateOf("") }
+
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedTextField(
+                value = phoneNumber,
+                onValueChange = { newPhoneNumber ->
+                    phoneNumber = newPhoneNumber
+                },
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text(text = "Phone Number") }
+            )
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "Sign in")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewPhoneAuthScreen() {
+    ConnectMeTheme {
+        PhoneAuthScreen(
+            modifier = Modifier
+                .background(color = MaterialTheme.colorScheme.background)
+        )
+    }
+}
